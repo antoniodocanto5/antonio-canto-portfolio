d8c5f31e2d2ff9010e64963684818de69566c734
antoniodocanto5 <antoniodocanto5@gmail.com>
2025-05-22 22:31:33 -0100

    Estrutura Inicial do projeto, em Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'cosmic-blue': '#0a0e2a',
+        'neon-blue': '#00ddeb',
+        'star-white': '#e0e7ff',
+      },
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
